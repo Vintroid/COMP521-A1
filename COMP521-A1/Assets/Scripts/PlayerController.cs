5903603e0db010ad67d3b366bcc54f23d4277cc8
Vincent St-Pierre <vstpierre1@gmail.com>
2023-09-15 02:41:13 -0400

    Ground + Controller + Camera commit

---
+++ COMP521-A1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{ 
+    // How fast the character will move along the map
+    [SerializeField] public float speed;
+    [SerializeField] public GameObject cam;
+    [SerializeField] public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Updating the player's position depending on keyboard inputs and speed.
+        // Camera transform is used so that player movement follow camera movement.
+        // Y component of camera is subtracted so that the player does not ascend or descend while moving.
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.MovePosition(rb.transform.position + (cam.transform.forward - new Vector3(0,cam.transform.forward.y,0)) * Time.deltaTime * speed);
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            rb.MovePosition(rb.transform.position - (cam.transform.forward - new Vector3(0, cam.transform.forward.y, 0)) * Time.deltaTime * speed);
+        }
+
+        // Rotating player with keys A & D.
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.MovePosition(rb.transform.position + cam.transform.right* Time.deltaTime * speed);
+        }
+
+        if(Input.GetKey(KeyCode.A)) 
+        {
+            rb.MovePosition(rb.transform.position - cam.transform.right* Time.deltaTime * speed);
+        }
+    }
+}
