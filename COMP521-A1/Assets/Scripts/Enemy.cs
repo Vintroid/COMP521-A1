11c1ad75672c2c725e56921dca9c696a5c6a7fc9
Vincent St-Pierre <vstpierre1@gmail.com>
2024-01-19 21:18:43 -0500

    Update Enemy.cs

--- COMP521-A1/Assets/Scripts/Enemy.cs
+++ COMP521-A1/Assets/Scripts/Enemy.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    // We want access to the eventManager functions from this class.
-    [SerializeField] EventManager eventManager;
+    // We want access to the GameManager functions from this class.
+    [SerializeField] GameManager gameManager;
 
     // Enemy body
-    [SerializeField] GameObject enemy;
+    [SerializeField] RigidBody rb;
+
+    // Enemy health
+    private int health = 2;
+    
+    // Movement type
+    int movementType = 0;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.velocity = new Vector2(-1f,0f);
     }
 
     // Update is called once per frame
@@ -22,10 +29,29 @@ public class Enemy : MonoBehaviour
         
     }
 
-    // Behaviour when the enemy is destroyed
-    public void OnDestroyed()
+    // Behaviour when the enemy is hit
+    public void OnHit()
     {
-        eventManager.EnemyDown();
-        GameObject.Destroy(enemy);
+       if(health>=0){
+           health--;
+       }
+       else
+       {
+           OnDestroyed();
+       }
+    }
+
+    // Behaviour when the enemy is destroyed
+    private void OnDestroyed()
+    {
+        gameManager.EnemyDown(this);
+        GameObject.Destroy(this);
+    }
+
+    private void OnCollisionEnter(Collision collision){
+        if(Collision.Collider.gameObject.Layer = "EnemyWall")
+        {
+            GameObject.Destroy(this);
+        }
     }
 }
