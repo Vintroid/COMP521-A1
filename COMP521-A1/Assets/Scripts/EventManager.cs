fe184963fe8fcc33bfc90a4d02ee2c80b09a2980
Vincent St-Pierre <vstpierre1@gmail.com>
2024-01-19 21:33:22 -0500

    Update EventManager.cs

--- COMP521-A1/Assets/Scripts/EventManager.cs
+++ COMP521-A1/Assets/Scripts/EventManager.cs
@@ -6,24 +6,13 @@ using UnityEngine;
 public class EventManager : MonoBehaviour
 {
     // Audio Clips
-    [SerializeField] AudioClip enemyDead;
-    [SerializeField] AudioClip enemyDownWin;
 
     // Game Objects
-    [SerializeField] Pistol pistol;
-    [SerializeField] PlayerController player;
-    [SerializeField] CanyonGrid canyonGrid;
-    [SerializeField] GameObject combatWall1;
-    [SerializeField] GameObject combatWall2;
-    [SerializeField] GameObject enemy;
-    [SerializeField] GameObject combatLine1;
-    [SerializeField] GameObject combatLine2;
-    [SerializeField] GameObject theRock1;
-    [SerializeField] GameObject theRock2;
-    [SerializeField] GameObject bridge1;
-    [SerializeField] GameObject bridge2;
 
-    // Booleans
+    // Game Fields
+    public float timer = 0f;
+    public int powerupRate = 2;
+    public int weaponRate = 2;
 
     // keep track of if the bullet is flying or not
     public bool firing = false;
@@ -38,28 +27,13 @@ public class EventManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.DeltaTime;
     }
 
     public void EnemyDown()
     {
-        // Modifying the scene after combat area completed.
-        AudioSource.PlayClipAtPoint(enemyDead,transform.position);
-        player.transform.position = new Vector3(-60.0f, 0f, 35.0f);
-        pistol.gameObject.SetActive(false);
-        combatWall2.gameObject.SetActive(false);
-        combatWall1.gameObject.SetActive(false);
-        combatLine1.gameObject.SetActive(false);
-        combatLine2.gameObject.SetActive(false);
-        theRock1.gameObject.SetActive(true);
-        theRock2.gameObject.SetActive(true);
-        bridge1.gameObject.SetActive(true);
-        bridge2.gameObject.SetActive(true);
-        AudioSource.PlayClipAtPoint(enemyDownWin,transform.position);
-
-        // Creating the paths in the canyon area
-        canyonGrid.InstantiatePaths();
-        
+        // Enemies can drop weapons and powerups
+        int randomInt = Random.
     }
 
     public void CombatStart()
